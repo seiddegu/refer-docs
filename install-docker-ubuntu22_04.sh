828a0d96a762db3d5059063aacd16cf2dc944b28
Seid Degu <seiddegu@gmail.com>
2023-08-10 09:36:51 +0300

    Create install-docker-ubuntu22_04.sh

---
+++ install-docker-ubuntu22_04.sh
@@ -0,0 +1,10 @@
+ sudo apt install apt-transport-https ca-certificates curl software-properties-common
+ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+ echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+ apt update
+ apt-cache policy docker-ce
+ apt install docker-ce
+ 
+ #Installation Check
+ systemctl is-active docker
+ systemctl status docker
