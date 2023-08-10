b44ce74608cef98e081c1d87b9faf6ca94d07fff
Seid Degu <seiddegu@gmail.com>
2023-08-10 09:45:10 +0300

    Update and rename install-docker-ubuntu22_04.sh to install_docker_annd_compose_ubuntu22-04.sh
    compose installation procedures update

--- install_docker_annd_compose_ubuntu22-04.sh
+++ install_docker_annd_compose_ubuntu22-04.sh
@@ -1,3 +1,4 @@
+ ###### Docker CE Installation
  sudo apt install apt-transport-https ca-certificates curl software-properties-common
  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
  echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
@@ -8,3 +9,13 @@
  #Installation Check
  systemctl is-active docker
  systemctl status docker
+
+
+ ###### Docker compose Installation
+ curl -SL https://github.com/docker/compose/releases/download/v2.3.3/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
+ chmod +x ~/.docker/cli-plugins/docker-compose
+ cd  ~/.docker/cli-plugins/
+ #to make it globally available via PATH
+ cp docker-compose /usr/bin/
+ docker-compose --version
+ docker compose version
